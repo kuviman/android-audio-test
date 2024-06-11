5f9af9940e6513944efc584acaec2e70d23d7253
kuviman <kuviman@gmail.com>
2024-06-11 19:07:38 +0400

    Smaller

--- src/lib.rs
+++ src/lib.rs
@@ -1,111 +1,237 @@
-use std::sync::Mutex;
-
-use winit::{
-    event::{ElementState, KeyEvent, Touch, TouchPhase, WindowEvent},
-    window::{Window, WindowAttributes},
+use android_activity::{
+    input::{InputEvent, KeyAction, KeyEvent, KeyMapChar, MotionAction},
+    AndroidApp, InputStatus, MainEvent, PollEvent,
 };
+use log::info;
 
-struct App {
-    window: Option<Window>,
-    audio: geng_audio::Audio,
-    sound: geng_audio::Sound,
-}
-
-impl App {
-    fn new() -> Self {
-        let audio = geng_audio::Audio::new().unwrap();
-        let data = load_audio_file();
-        let sound = futures::executor::block_on(audio.decode(data)).unwrap();
-        println!("app initialized");
-        Self {
-            window: None,
-            audio,
-            sound,
-        }
-    }
-
-    fn create_window(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.window.replace(
-            event_loop
-                .create_window(Window::default_attributes())
-                .unwrap(),
-        );
-        println!("window created");
-    }
-}
-
-impl winit::application::ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.create_window(event_loop);
-    }
-
-    fn window_event(
-        &mut self,
-        _event_loop: &winit::event_loop::ActiveEventLoop,
-        _window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
-    ) {
-        println!("{event:?}");
-        if let WindowEvent::Touch(Touch {
-            phase: TouchPhase::Started,
-            ..
-        })
-        | WindowEvent::KeyboardInput {
-            event:
-                KeyEvent {
-                    state: ElementState::Pressed,
-                    repeat: false,
-                    ..
-                },
-            ..
-        }
-        | WindowEvent::MouseInput {
-            state: ElementState::Pressed,
-            ..
-        } = event
-        {
-            self.sound.play();
-        }
-    }
-}
-
-#[cfg(target_os = "android")]
-static APP: Mutex<Option<android_activity::AndroidApp>> = Mutex::new(None);
-
-#[cfg(not(target_os = "android"))]
-fn load_audio_file() -> Vec<u8> {
-    std::fs::read("assets/sound.wav").unwrap()
-}
-
-#[cfg(target_os = "android")]
-fn load_audio_file() -> Vec<u8> {
-    use std::io::Read;
-    let app = APP.lock().unwrap().as_ref().unwrap().clone();
-    let asset_manager = app.asset_manager();
-    let path = std::ffi::CString::new("sound.wav").unwrap();
-    let mut asset = asset_manager.open(path.as_c_str()).unwrap();
-    let mut buffer = Vec::new();
-    asset.read_to_end(&mut buffer).unwrap();
-    buffer
-}
-
-#[cfg(target_os = "android")]
 #[no_mangle]
-fn android_main(app: android_activity::AndroidApp) {
-    APP.lock().unwrap().replace(app);
-    run();
+fn android_main(app: AndroidApp) {
+    android_logger::init_once(android_logger::Config::default().with_min_level(log::Level::Info));
+
+    let audio = geng_audio::Audio::new().unwrap();
+    let sound = {
+        use std::io::Read;
+        let asset_manager = app.asset_manager();
+        let path = std::ffi::CString::new("sound.wav").unwrap();
+        let mut asset = asset_manager.open(path.as_c_str()).unwrap();
+        let mut buffer = Vec::new();
+        asset.read_to_end(&mut buffer).unwrap();
+        futures::executor::block_on(audio.decode(buffer)).unwrap()
+    };
+
+    let mut quit = false;
+    let mut redraw_pending = true;
+    let mut native_window: Option<ndk::native_window::NativeWindow> = None;
+
+    let mut combining_accent = None;
+
+    while !quit {
+        app.poll_events(
+            Some(std::time::Duration::from_secs(1)), /* timeout */
+            |event| {
+                match event {
+                    PollEvent::Wake => {
+                        info!("Early wake up");
+                    }
+                    PollEvent::Timeout => {
+                        info!("Timed out");
+                        // Real app would probably rely on vblank sync via graphics API...
+                        redraw_pending = true;
+                    }
+                    PollEvent::Main(main_event) => {
+                        info!("Main event: {:?}", main_event);
+                        match main_event {
+                            MainEvent::SaveState { saver, .. } => {
+                                saver.store("foo://bar".as_bytes());
+                            }
+                            MainEvent::Pause => {}
+                            MainEvent::Resume { loader, .. } => {
+                                if let Some(state) = loader.load() {
+                                    if let Ok(uri) = String::from_utf8(state) {
+                                        info!("Resumed with saved state = {uri:#?}");
+                                    }
+                                }
+                            }
+                            MainEvent::InitWindow { .. } => {
+                                native_window = app.native_window();
+                                redraw_pending = true;
+                            }
+                            MainEvent::TerminateWindow { .. } => {
+                                native_window = None;
+                            }
+                            MainEvent::WindowResized { .. } => {
+                                redraw_pending = true;
+                            }
+                            MainEvent::RedrawNeeded { .. } => {
+                                redraw_pending = true;
+                            }
+                            MainEvent::InputAvailable { .. } => {
+                                redraw_pending = true;
+                            }
+                            MainEvent::ConfigChanged { .. } => {
+                                info!("Config Changed: {:#?}", app.config());
+                            }
+                            MainEvent::LowMemory => {}
+
+                            MainEvent::Destroy => quit = true,
+                            _ => { /* ... */ }
+                        }
+                    }
+                    _ => {}
+                }
+
+                if redraw_pending {
+                    if let Some(native_window) = &native_window {
+                        redraw_pending = false;
+
+                        // Handle input, via a lending iterator
+                        match app.input_events_iter() {
+                            Ok(mut iter) => loop {
+                                info!("Checking for next input event...");
+                                if !iter.next(|event| {
+                                    match event {
+                                        InputEvent::KeyEvent(key_event) => {
+                                            let combined_key_char = character_map_and_combine_key(
+                                                &app,
+                                                key_event,
+                                                &mut combining_accent,
+                                            );
+                                            info!("KeyEvent: combined key: {combined_key_char:?}")
+                                        }
+                                        InputEvent::MotionEvent(motion_event) => {
+                                            println!("action = {:?}", motion_event.action());
+                                            match motion_event.action() {
+                                                MotionAction::Up => {
+                                                    let pointer = motion_event.pointer_index();
+                                                    let pointer =
+                                                        motion_event.pointer_at_index(pointer);
+                                                    let x = pointer.x();
+                                                    let y = pointer.y();
+
+                                                    println!("POINTER UP {x}, {y}");
+
+                                                    sound.play();
+
+                                                    if x < 200.0 && y < 200.0 {
+                                                        println!("Requesting to show keyboard");
+                                                        app.show_soft_input(true);
+                                                    }
+                                                }
+                                                _ => {}
+                                            }
+                                        }
+                                        InputEvent::TextEvent(state) => {
+                                            info!("Input Method State: {state:?}");
+                                        }
+                                        _ => {}
+                                    }
+
+                                    info!("Input Event: {event:?}");
+                                    InputStatus::Unhandled
+                                }) {
+                                    info!("No more input available");
+                                    break;
+                                }
+                            },
+                            Err(err) => {
+                                log::error!("Failed to get input events iterator: {err:?}");
+                            }
+                        }
+
+                        info!("Render...");
+                        dummy_render(native_window);
+                    }
+                }
+            },
+        );
+    }
 }
 
-pub fn run() {
-    let event_loop = {
-        let mut builder = winit::event_loop::EventLoop::builder();
-        #[cfg(target_os = "android")]
-        {
-            use winit::platform::android::EventLoopBuilderExtAndroid;
-            builder.with_android_app(APP.lock().unwrap().as_ref().unwrap().clone());
+/// Tries to map the `key_event` to a `KeyMapChar` containing a unicode character or dead key accent
+///
+/// This shows how to take a `KeyEvent` and look up its corresponding `KeyCharacterMap` and
+/// use that to try and map the `key_code` + `meta_state` to a unicode character or a
+/// dead key that be combined with the next key press.
+fn character_map_and_combine_key(
+    app: &AndroidApp,
+    key_event: &KeyEvent,
+    combining_accent: &mut Option<char>,
+) -> Option<KeyMapChar> {
+    let device_id = key_event.device_id();
+
+    let key_map = match app.device_key_character_map(device_id) {
+        Ok(key_map) => key_map,
+        Err(err) => {
+            log::error!("Failed to look up `KeyCharacterMap` for device {device_id}: {err:?}");
+            return None;
         }
-        builder.build().unwrap()
     };
-    let mut app = App::new();
-    event_loop.run_app(&mut app).unwrap();
+
+    match key_map.get(key_event.key_code(), key_event.meta_state()) {
+        Ok(KeyMapChar::Unicode(unicode)) => {
+            // Only do dead key combining on key down
+            if key_event.action() == KeyAction::Down {
+                let combined_unicode = if let Some(accent) = combining_accent {
+                    match key_map.get_dead_char(*accent, unicode) {
+                        Ok(Some(key)) => {
+                            info!("KeyEvent: Combined '{unicode}' with accent '{accent}' to give '{key}'");
+                            Some(key)
+                        }
+                        Ok(None) => None,
+                        Err(err) => {
+                            log::error!("KeyEvent: Failed to combine 'dead key' accent '{accent}' with '{unicode}': {err:?}");
+                            None
+                        }
+                    }
+                } else {
+                    info!("KeyEvent: Pressed '{unicode}'");
+                    Some(unicode)
+                };
+                *combining_accent = None;
+                combined_unicode.map(|unicode| KeyMapChar::Unicode(unicode))
+            } else {
+                Some(KeyMapChar::Unicode(unicode))
+            }
+        }
+        Ok(KeyMapChar::CombiningAccent(accent)) => {
+            if key_event.action() == KeyAction::Down {
+                info!("KeyEvent: Pressed 'dead key' combining accent '{accent}'");
+                *combining_accent = Some(accent);
+            }
+            Some(KeyMapChar::CombiningAccent(accent))
+        }
+        Ok(KeyMapChar::None) => {
+            // Leave any combining_accent state in tact (seems to match how other
+            // Android apps work)
+            info!("KeyEvent: Pressed non-unicode key");
+            None
+        }
+        Err(err) => {
+            log::error!("KeyEvent: Failed to get key map character: {err:?}");
+            *combining_accent = None;
+            None
+        }
+    }
+}
+
+/// Post a NOP frame to the window
+///
+/// Since this is a bare minimum test app we don't depend
+/// on any GPU graphics APIs but we do need to at least
+/// convince Android that we're drawing something and are
+/// responsive, otherwise it will stop delivering input
+/// events to us.
+fn dummy_render(native_window: &ndk::native_window::NativeWindow) {
+    unsafe {
+        let mut buf: ndk_sys::ANativeWindow_Buffer = std::mem::zeroed();
+        let mut rect: ndk_sys::ARect = std::mem::zeroed();
+        ndk_sys::ANativeWindow_lock(
+            native_window.ptr().as_ptr() as _,
+            &mut buf as _,
+            &mut rect as _,
+        );
+        // Note: we don't try and touch the buffer since that
+        // also requires us to handle various buffer formats
+        ndk_sys::ANativeWindow_unlockAndPost(native_window.ptr().as_ptr() as _);
+    }
 }
